84c8d50121e65b17a9efcca02fdbeeadbe71dd16
augustoizepon <augustom.izepon@gmail.com>
2023-10-31 15:33:40 -0300

    commit cadastro e select

--- src/index.js
+++ src/index.js
@@ -8,15 +8,23 @@ app.use(express.json())
 
 
 app.get('/pessoas', async (req, res) => {
-   const pessoaslist = await knex('pessoas').select('*')
-   res.json(pessoaslist)
+    try {
+        const pessoaslist = await knex('pessoas').select('*')
+        res.json(pessoaslist)
+    } catch (error) {
+        console.log(error.message)
+    }
 })
 
-app.post('/pessoas', async(req,  res) =>{
-    const {nome, senha} = req.body
-    const senhaCrypt =  await bcrypt.hash(senha, 10)
-    knex('pessoas').insert({nome: nome, senha: senhaCrypt})
-    res.json('criado com sucesso')
+app.post('/pessoas', async (req, res) => {
+    try {
+        const { nome, senha } = req.body
+        const senhaCrypt = await bcrypt.hash(senha, 10)
+        knex('pessoas').insert({ nome: nome, senha: senhaCrypt })
+        res.json('criado com sucesso')
+    } catch (error) {
+        console.log(error.message)
+    }
 })
 
 
